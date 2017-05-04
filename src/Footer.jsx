8b09342cde0fd7423a4f073a840ca03cd36227d3
ioanclaudiusolomon <claudiusolomon008@gmail.com>
2017-05-04 10:23:02 +0300

    Start.jsx: changed the welcome message; Footer.jsx: changed the copyright message.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,13 +4,16 @@ import { Grid, Row, Col } from 'react-bootstrap';
 class Footer extends Component {
     render() {
         return (
-                <Grid className="footer">
-                    <Row>
-                        <Col xs={12} sm={12} md={12} lg={12}>
-                            <p>Copyright &copy; 2017-present, Solomon Ioan Claudiu</p>
-                        </Col>
-                    </Row>
-                </Grid>
+            <Grid className="footer">
+                <Row>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <p>
+                            Copyright &copy; 2017-present, Solomon Ioan Claudiu<br/>
+                            <small>This is a simple web application created with the sole purpose of learning.</small>
+                        </p>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
--- src/Start.jsx
+++ src/Start.jsx
@@ -15,7 +15,6 @@ class Start extends Component {
         return (
             <Jumbotron>
                 <h1>Hello!</h1>
-                <p>This is a simple web application created with the sole purpose of learning.</p>
                 <p>You can plan a meeting as an annonymous user or you can register and you will have access to the history of your meetings.</p>
                 <p><Button bsStyle="primary" onClick={this.handlePlanMeetingClick}>Plan meeting</Button></p>
             </Jumbotron>
